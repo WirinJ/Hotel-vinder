620028c9a4e522414abaf0eebbf65713afcf0be1
W <2135017@talnet.nl>
2022-02-23 17:18:47 +0100

    Add views.

--- website/hotel_data/views.py
+++ website/hotel_data/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from hotel_data.models import Hotel, City
+
 # Create your views here.
+def index(request):
+    city_list = City.objects.all()
+    context = {
+        'city_list': city_list,
+    }
+    return render(request, 'index.html', context)
+
+
+def hotels(request):
+    city_id = request.GET.get('city_id')
+    hotel_list = Hotel.objects.filter(city_id = city_id)
+    context = {
+        'hotel_list': hotel_list,
+    }
+    return render(request, 'index.html', context)
