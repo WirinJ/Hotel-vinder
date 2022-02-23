ec18cb7a090fac947badfaef0fb5974bc1019237
W <2135017@talnet.nl>
2022-02-23 17:18:20 +0100

    Create urls.

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hotel_data import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('hotels/', views.hotels),
 ]
