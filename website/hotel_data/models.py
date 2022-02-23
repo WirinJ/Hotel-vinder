f4ba829e85f7b6a4371b31b59e4a1f999345eaa7
W <2135017@talnet.nl>
2022-02-23 17:12:57 +0100

    Modify the model.

--- website/hotel_data/models.py
+++ website/hotel_data/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=125)
 
+    def __str__(self):
+        return self.name 
+
 class Hotel(models.Model):
     name = models.CharField(max_length=125)
+    hotel_tag = models.CharField(max_length=25)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
